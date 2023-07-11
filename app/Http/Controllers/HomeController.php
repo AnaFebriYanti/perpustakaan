3c9ffe07c65de576544bb48d98b0bfdc5226ee1f
AnaFebriYanti <anafebriyanti844@gmail.com>
2023-07-11 22:05:12 +0700

    ubah home buku

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -21,8 +22,14 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        $title = 'home';
+        $pages = 'home';
+
+        $res_kategori_buku = DB::select('select * from kategori_buku');
+        $res_buku = DB::select('select * from buku');
+        return view('home',compact('title', 'pages', 'res_kategori_buku','res_buku'))
+            ->with('i', ($request->input('page', 1) - 1) * 5);
     }
 }
